36cbe7c9511d76255284c66ac007a24e731fb4c6
William Lundgren <william.lundgren94@gmail.com>
2023-05-02 19:45:14 +0200

    Override theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css';
+import {Stack, Button} from '@mui/material';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Spotify Clone by William Lundgren</h1>
+			<Stack spacing={2} direction="row">
+				<Button variant="text">Text</Button>
+				<Button variant="contained">Contained</Button>
+				<Button variant="outlined">Outlined</Button>
+			</Stack>
 		</div>
 	);
 }
