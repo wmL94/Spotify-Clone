81e037cb9baaf536ea666bb38d50976b515aaf1f
William Lundgren <william.lundgren94@gmail.com>
2023-05-02 19:48:47 +0200

    imported themeOptions main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { ThemeProvider } from '@mui/system';
+import { themeOptions } from './Theme/Material-theme';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={themeOptions}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
