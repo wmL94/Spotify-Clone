8067766bad9cb4f44610fd91026a0ad580183e06
William Lundgren <william.lundgren94@gmail.com>
2023-05-02 19:59:56 +0200

    changed div to box

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import './App.css';
-import {Stack, Button} from '@mui/material';
+import {Stack, Button, Box} from '@mui/material';
 
-function App() {
+function App({spotifyApi}) {
 	return (
-		<div className="App">
+		<Box className="App">
 			<h1>Spotify Clone by William Lundgren</h1>
 			<Stack spacing={2} direction="row">
 				<Button variant="text">Text</Button>
 				<Button variant="contained">Contained</Button>
 				<Button variant="outlined">Outlined</Button>
 			</Stack>
-		</div>
+		</Box>
 	);
 }
 
