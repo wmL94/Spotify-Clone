5a437e593ac5696f192a15a4be2bc879f52bd23b
William Lundgren <william.lundgren94@gmail.com>
2023-05-02 20:40:49 +0200

    removed stack and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css';
-import {Stack, Button, Box} from '@mui/material';
+import { Box } from '@mui/material';
+import Login from './pages/Login';
 
 function App({spotifyApi}) {
 	return (
 		<Box className="App">
 			<h1>Spotify Clone by William Lundgren</h1>
-			<Stack spacing={2} direction="row">
-				<Button variant="text">Text</Button>
-				<Button variant="contained">Contained</Button>
-				<Button variant="outlined">Outlined</Button>
-			</Stack>
+			<Login />
 		</Box>
 	);
 }
