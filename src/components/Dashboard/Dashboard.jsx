9b8dd9736a41ccd4dfa7e87afb29d6590de67dab
William Lundgren <william.lundgren94@gmail.com>
2023-05-03 18:54:00 +0200

    centered content

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,8 +3,8 @@ import {Routes, Route} from 'react-router-dom';
 import Home from '../../pages/Home';
 
 const Dashboard = ( {spotifyApi} ) => {
-    return <Box>
-        <Box>
+    return <Box sx={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column'}}>
+        <Box sx={{flex: 1, overflowY: 'auto', display: 'flex' }}>
             <Routes>
                 <Route path="/playlist/:id" element={<div>Playlist</div>} />
                 <Route path="/library" element={<div>library</div>} />
