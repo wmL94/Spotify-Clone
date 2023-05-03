54137c43f025f84485b93c89a7872b6c9b842fad
William Lundgren <william.lundgren94@gmail.com>
2023-05-03 18:35:44 +0200

    removed some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import { Box } from '@mui/material';
 import Login from './pages/Login';
+import Home from './pages/Home';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App({spotifyApi}) {
 	return (
 		<Box className="App">
-			<h1>Spotify Clone by William Lundgren</h1>
-			<Login />
+			<Home/>
 		</Box>
 	);
 }
