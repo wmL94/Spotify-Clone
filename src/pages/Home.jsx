d34b820d3fef591089a9e67d060efab5c6e4473d
William Lundgren <william.lundgren94@gmail.com>
2023-05-03 18:35:08 +0200

    created a home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import { Box, Button } from '@mui/material';
+
+const Home = () => {
+	return (
+		<Box
+			sx={{
+				flex: 1,
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				flexDirection: 'column',
+				gap: 5
+			}}
+		>
+			<img src="/hero.jpg" style={{ maxHeight: '25%', maxWidth: '25%' }} alt="William Lundgren" />
+			<Button
+				size="large"
+				variant="contained"
+				onClick={() => (window.location.href = 'mailto:william.lundgren94@gmail.com')}
+			>
+				Kontakta mig
+			</Button>
+		</Box>
+	);
+};
+
+export default Home;
